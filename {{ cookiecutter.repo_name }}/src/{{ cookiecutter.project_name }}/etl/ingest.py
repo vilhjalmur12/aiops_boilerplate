12e705b8159f203e97835396b9e4184a145ce3c2
Vilhjalmur (Villi) <vilhjalmssonrn@gmail.com>
2025-12-09 14:18:00 +0100

    etl structure

---
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.project_name }}/etl/ingest.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from ..io.data_sources import load_raw_data
+
+
+def ingest():
+    return load_raw_data()
---
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.project_name }}/etl/materialize.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+
+def materialize(data) -> None:
+    _ = data  # noqa: F841
